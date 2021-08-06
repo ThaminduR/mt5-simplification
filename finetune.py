55afcd4e445ffa872b3c2a59b3ea1af90d7ba558
Thamindu Randil <45028707+ThaminduR@users.noreply.github.com>
2021-08-06 21:19:42 +0530

    change save_Steps to 1000

--- finetune.py
+++ finetune.py
@@ -534,7 +534,7 @@ def main():
             checkpoint = training_args.resume_from_checkpoint
         elif last_checkpoint is not None:
             checkpoint = last_checkpoint
-        train_result = trainer.train(resume_from_checkpoint=checkpoint)
+        train_result = trainer.train(resume_from_checkpoint=checkpoint, save_steps=1000)
         trainer.save_model()  # Saves the tokenizer too for easy upload
 
         metrics = train_result.metrics
